cceab4b7c9d2347bb46d0f2d3271c2d73ce86add
riz <riz@rizvn.com>
2018-08-03 19:30:46 +0100

    .

--- src/test/java/com/rizvn/ConsumerTest.java
+++ src/test/java/com/rizvn/ConsumerTest.java
@@ -1,9 +1,13 @@
 package com.rizvn;
 
 import org.apache.tomcat.jdbc.pool.DataSource;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -35,34 +39,32 @@ public class ConsumerTest {
 
   @Test
   public void multipleConsumers() throws Exception{
+    final List<Long> ids = new ArrayList<>();
+    MessageHandler handler = (message -> ids.add(message.messageId));
 
-    MessageHandler handler = (message -> message.toString());
-
-    Consumer consumer1 = new Consumer("1", dataSource, "topic1", handler, 1, TimeUnit.MILLISECONDS);
-    Consumer consumer2 = new Consumer("2", dataSource, "topic1", handler, 1, TimeUnit.MILLISECONDS);
-    Consumer consumer3 = new Consumer("3", dataSource, "topic1", handler, 1, TimeUnit.MILLISECONDS);
-    Consumer consumer4 = new Consumer("4", dataSource, "topic1", handler, 1, TimeUnit.MILLISECONDS);
-    Consumer consumer5 = new Consumer("5", dataSource, "topic1", handler, 1, TimeUnit.MILLISECONDS);
-    Consumer consumer6 = new Consumer("5", dataSource, "topic1", handler, 1, TimeUnit.MILLISECONDS);
-    Consumer consumer7 = new Consumer("5", dataSource, "topic1", handler, 1, TimeUnit.MILLISECONDS);
-    Consumer consumer8 = new Consumer("5", dataSource, "topic1", handler, 1, TimeUnit.MILLISECONDS);
+    Consumer consumer1 = new Consumer("1", dataSource, "topic1", handler, 50, TimeUnit.MILLISECONDS);
+    Consumer consumer2 = new Consumer("2", dataSource, "topic1", handler, 50, TimeUnit.MILLISECONDS);
+    Consumer consumer3 = new Consumer("3", dataSource, "topic1", handler, 50, TimeUnit.MILLISECONDS);
+    Consumer consumer4 = new Consumer("4", dataSource, "topic1", handler, 50, TimeUnit.MILLISECONDS);
     Unlocker unlocker = new Unlocker(dataSource, 100, TimeUnit.MILLISECONDS, 3l);
 
     consumer1.start();
     consumer2.start();
     consumer3.start();
     consumer4.start();
-    consumer5.start();
-    consumer6.start();
-    consumer7.start();
-    consumer8.start();
 
     unlocker.start();
 
-    for(int i=0; ; i++){
+    for(int i=0; i < 100; i++){
     //  Thread.sleep(100);
       producer.produce("topic1", "Hello world "+ i);
     }
 
+    Thread.sleep(5000);
+
+    List<Long> sortedList = new ArrayList(ids);
+    Collections.sort(sortedList);
+
+    Assert.assertEquals(sortedList, ids);
   }
 }
